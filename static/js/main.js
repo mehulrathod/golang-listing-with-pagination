6b1be83b40512c6f50bc8932a5f78e23667249a7
mehul rathod <mehulrathod@mehuls-Mac-mini.local>
2021-04-09 12:46:27 +0530

    updated js previous and next logic

--- static/js/main.js
+++ static/js/main.js
@@ -8,7 +8,7 @@ const getList = (page = 1, pageSize  = 10) => {
 const prepareList = (result) => {
     preparePaging();
     const listContainer = document.querySelector(".employee-list");
-    listContainer.innerHTML = '<tr>' + result.data.map(employee => `<td>${employee.name}</td><td>${employee.city}</td>`).join("</tr><tr>")
+    listContainer.innerHTML = '<tr>' + result.data.map(employee => `<td>${employee.id}</td><td>${employee.name}</td><td>${employee.city}</td>`).join("</tr><tr>")
 }
 const pageChange = (pageNum) => {
     currentPage = pageNum;
@@ -18,10 +18,21 @@ const preparePaging = () => {
     const pagingContainer = document.querySelector(".pagination");
     // const pageSize = 10;
     // const numOfPages = Math.floor(1000/pageSize);
+
     let html = '';
-    for(let i=currentPage - 1; i<= currentPage + 1; i++) {
+    if(currentPage >= 3) {
+        html += `<li class="page-item ">
+                    <a class="page-link" href="javascript:void(0)" onclick="pageChange(${currentPage - 1})">Previous</a>
+                </li>`;
+    }
+    for(let i=currentPage === 1 ? 1 : currentPage - 1; i<= currentPage + (currentPage === 1 ? 2 : 1); i++) {
         html += `<li class="page-item ${currentPage === i ? 'active' : ''}">
                     <a class="page-link" href="javascript:void(0)" onclick="pageChange(${i})">${i}</a>
+                </li>`;
+    }
+    if(3 <= currentPage ) {
+        html += `<li class="page-item ">
+                    <a class="page-link" href="javascript:void(0)" onclick="pageChange(${currentPage + 1})">Next</a>
                 </li>`;
     }
 
